942dc200fce623ebf9681e90470d62004ac79af2
Luis Cabrera Benito <parzibyte@gmail.com>
2022-01-31 10:44:24 -0600

    Filtro para timestamp a fecha

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import '@mdi/font/css/materialdesignicons.css'
 Vue.use(Buefy);
 Vue.config.productionTip = false
+const formateadorFecha = new Intl.DateTimeFormat('es-MX', { dateStyle: 'medium', timeStyle: "medium" });
+Vue.filter("timestampAFecha", timestamp => {
+  if (!timestamp) {
+    return "";
+  }
+  return formateadorFecha.format(new Date(timestamp));
+});
 
 new Vue({
   render: h => h(App),
